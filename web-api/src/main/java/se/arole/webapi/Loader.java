a337ed995595097f72906b4740210cf5a3369862
hejenstedt <l.hejenstedt@gmail.com>
2016-01-19 14:35:17 +0100

    Loader class to start jax-rs app. UserResource for /user path

---
+++ web-api/src/main/java/se/arole/webapi/Loader.java
@@ -0,0 +1,9 @@
+package se.arole.webapi;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class Loader extends Application{
+
+}
--- web-api/src/main/java/se/arole/webapi/resources/UserResource.java
+++ web-api/src/main/java/se/arole/webapi/resources/UserResource.java
@@ -1,25 +1,38 @@
 package se.arole.webapi.resources;
 
+import java.net.URI;
+import java.util.Collection;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 
+import se.arole.api.controller.UserController;
 import se.arole.api.resource.User;
 
 @Path("user")
-public class UserResource {
+public final class UserResource {
+
+	private UserController userController;
+
+	@Context
+	private UriInfo uriInfo;
 
 	@GET
 	public Response getAll() {
-
-		return null;
+		Collection<User> users = userController.getAll();
+		return Response.ok(users).build();
 	}
 
 	@POST
 	public Response createUser(User user) {
+		User createdUser = userController.create(user);
 
-		return null;
+		URI location = uriInfo.getAbsolutePathBuilder().path("" + createdUser.getUserId()).build();
+		return Response.created(location).build();
 	}
 
 }
