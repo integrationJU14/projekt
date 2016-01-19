0ebc80977690616bb4f4e159495f2634f0e2546c
Holstad <ola.holstad@gmail.com>
2016-01-19 14:00:43 +0100

    no userserviceimp

--- datalayer/src/main/java/se/arole/datalayer/services/UserServiceImp.java
+++ datalayer/src/main/java/se/arole/datalayer/services/UserServiceImp.java
@@ -1,40 +1,10 @@
 package se.arole.datalayer.services;
 
-import se.arole.api.resource.User;
-import se.arole.api.service.UserService;
 import se.arole.datalayer.repository.UserRepository;
 
-public class UserServiceImp implements UserService {
+public class UserServiceImp {
 	
 	private UserRepository userRepository;
-	
-	@Override
-	public void changeStatusUser(boolean arg0, long arg1) {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public User createUser(User user) {
-		return null;
-	}
-
-	@Override
-	public User getUser(long arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public User getUserByUsername(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public User updateUser(User arg0, long arg1) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
