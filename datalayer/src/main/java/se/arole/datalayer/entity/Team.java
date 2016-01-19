03709bf6bbd62a2a02be9ebb9e6bfca44520ae83
Holstad <ola.holstad@gmail.com>
2016-01-19 14:09:54 +0100

    Team

---
+++ datalayer/src/main/java/se/arole/datalayer/entity/Team.java
@@ -0,0 +1,19 @@
+package se.arole.datalayer.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Team {
+	@Id 
+	@GeneratedValue
+	private Long id;
+	@Column
+	private String name;
+	@Column
+	private Integer teamId;
+	
+
+}
