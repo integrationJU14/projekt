9ddc9b4253837e4524fe70ee1276b288fa61b405
woooopoodle <rstrom93@gmail.com>
2016-01-19 15:16:19 +0100

    IssueService /Rickard

---
+++ datalayer/src/main/java/se/arole/datalayer/services/IssueService.java
@@ -0,0 +1,16 @@
+package se.arole.datalayer.services;
+
+import java.util.List;
+import se.arole.datalayer.entity.Issue;
+
+public interface IssueService {
+	
+	Issue createIssue(Issue issue);
+	
+	Issue updateIssue(Issue issue, int issueId);
+	
+	void addIssueToWorkItem(int workItemId, Issue issue);
+	
+	List<Issue> workItemsWithIssues(/**/);
+
+}
